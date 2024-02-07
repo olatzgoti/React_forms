66628e4fdb654eae03abbc9ef92a83b08b61a952
olatz <gotiolatz@gmail.com>
2024-02-07 18:59:00 +0100

    router

--- react_form/src/App.jsx
+++ react_form/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Contact from './component/Contact'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
